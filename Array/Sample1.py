f9f363a6fd5b192f9e0cdff61591127b61f8148e
Better_DE <hoangtrhien203@gmail.com>
2023-01-06 15:24:17 +0700

    Format in Sample1.py

--- Array/Sample1.py
+++ Array/Sample1.py
@@ -1,17 +1,17 @@
-# This is array
-arr = [1,2,3,4,5]
+# This is Array
+arr = [1, 2, 3, 4, 5]
 
-# Create a Array
-arr1 = [] # Create a Array
+# Create an Array
+arr1 = [] # Create an Array
 n = int(input("Enter len of arr: "))
 for i in range(n):
     arr1.append(int(input(f"Enter the {i+1} element: ")))
 
-print(*arr1,sep=' ')
+print(*arr1, sep=' ')
 
 # Other way
 arr2 = []
 n = int(input("Enter len of arr: "))
 [arr2.append(int(input(f"Enter the {i+1} element: "))) for i in range(n)]
 
-print(*arr2,sep=" ")
+print(*arr2, sep=" ")
